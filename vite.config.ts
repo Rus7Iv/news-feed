1341706f916bd250ff439049b60333edc8cc88e0
Ruslan Ivanov <rusivary@gmail.com>
2024-09-13 11:55:44 +0400

    fixed aliases configuration errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
 import svgr from '@svgr/rollup'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
 import { defineConfig } from 'vite'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': resolve(__dirname, './src'),
     },
   },
 })
