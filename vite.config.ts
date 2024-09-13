66f4a1c46ea1e67d9c8e4c2b21980213a55fa996
Ruslan Ivanov <rusivary@gmail.com>
2024-09-13 11:47:12 +0400

    added aliases in config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import svgr from '@svgr/rollup'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
